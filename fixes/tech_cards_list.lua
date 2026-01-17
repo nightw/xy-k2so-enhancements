63b9fc4436441ea37179c79603f08b084354c78f
xyrc1 <75494811+xyrc1@users.noreply.github.com>
2026-01-17 01:09:16 +0000

    apply new tech card system

---
+++ fixes/tech_cards_list.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        ['mod'] = 'Paracelsin',
+        ['setting'] = settings.startup[xy-paracelsin-tech-card].value,
+        ['original_name'] = 'galvanization-science-pack',
+        ['short_name'] = 'galvanization',
+        ['import_location'] = 'paracelsin',
+    },
+    {
+        ['mod'] = 'secretas',
+        ['setting'] = true,-- change later
+        ['original_name'] = 'golden-research-data',
+        ['short_name'] = 'golden',
+        ['import_location'] = 'frozeta',
+    },
+    {
+        ['mod'] = 'secretas',
+        ['setting'] = true,-- change later
+        ['original_name'] = 'golden-research-data',
+        ['short_name'] = 'golden',
+        ['import_location'] = 'frozeta',
+    },
+    {
+        ['mod'] = 'corrundum',
+        ['setting'] = true,-- change later
+        ['original_name'] = 'electrochemical-science-pack',
+        ['short_name'] = 'electrochemical',
+        ['import_location'] = 'corrundum',
+    },
+}
